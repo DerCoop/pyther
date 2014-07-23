8667ef0f769e35f84233dd3662ea96a0b53f533e
Der Coop <dercoop@users.sourceforge.net>
2014-07-23 20:05:55 +0200

    second example plugin for pyther
    this is a example implementation of a unix shell command
    the python plugin is only a API for pyther

---
+++ plugins/ls.py
@@ -0,0 +1,29 @@
+"""
+    implementation of a system command
+
+    example implementation of the unix 'ls' command with all options
+
+    Written by DerCoop <dercoop@users.sourceforge.net>
+
+"""
+
+__author__ = 'coop'
+__license__ = 'GPLv2'
+
+
+import subprocess
+import shlex
+from plugin import Plugin
+
+
+class Ls(Plugin):
+    def __init__(self):
+        # do not print the default stuff from the main pyther plugin
+        pass
+
+    @staticmethod
+    def run(args):
+        cmd = ['ls']
+        if args:
+            cmd.extend(shlex.split(args))
+        print subprocess.check_output(cmd)
